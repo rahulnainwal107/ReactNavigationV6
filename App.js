fbcf7402bf18d4a6573034e97bb4cd220b821107
rnainwal <rnainwal@prometheusgroup.com>
2022-09-28 22:03:31 +0530

    Update for state-persistence.

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import "react-native-gesture-handler";
-import { StyleSheet, StatusBar } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, StatusBar, Linking } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Initial from "./src/navigations/Initial";
+import { PERSISTENCE_KEY } from "./src/utils/constants";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+        console.log("initialUrl ", initialUrl);
+        if (Platform.OS !== "web" && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          console.log("savedStateString ", savedStateString);
+          const state = savedStateString
+            ? JSON.parse(savedStateString)
+            : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      initialState={initialState}
+      onStateChange={(state) => console.log("state ", state)} // https://reactnavigation.org/docs/state-persistence
+    >
       <StatusBar barStyle="dark-content" backgroundColor="white" />
       <Initial />
     </NavigationContainer>
